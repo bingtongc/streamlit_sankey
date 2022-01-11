3c866e4c842a25b2d22b6a6d402f29ee40365cc0
Chua Bing Tong <bingtong.chua@my.corp.seagroup.com>
2022-01-11 16:25:18 +0800

    Add streamlit

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('hello')
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+df_five_step = pd.read_csv("C:\\Users\\bingtong.chua\\Desktop\\first_5_click.csv")
+st.write(pd.unique(df_five_step['first_step'])
